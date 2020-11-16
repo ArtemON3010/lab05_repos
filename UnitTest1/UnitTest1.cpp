041093fbd5583b4c2382fded62c22f849d3fdd5f
ArtemON3010 <artem.turko.ITIS.2020@lpnu.ua>
2020-11-16 23:14:19 +0200

    first branch_03 commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Лабораторна робота №5.2 (1)/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = A(1, 2, 3);
+			Assert::AreEqual(t, 12.);
+		}
+	};
+}
